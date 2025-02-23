f19c649877a1e8c36f06822bea7ebb41f8b6e643
somya-jangid <somyajangid78@gmail.com>
2025-02-24 01:16:46 +0530

    Add or update backend/routes/movies.js

---
+++ backend/routes/movies.js
@@ -0,0 +1,1 @@
+const express = require('express');const router = express.Router();router.get('/', (req, res) => { res.send('Get all movies'); });router.post('/', (req, res) => { res.send('Add a new movie'); });module.exports = router;
