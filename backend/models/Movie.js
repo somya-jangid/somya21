3cbc604443b05f1c9389a5f9b11ff0a31bd0d159
somya-jangid <somyajangid78@gmail.com>
2025-02-24 01:16:49 +0530

    Add or update backend/models/Movie.js

---
+++ backend/models/Movie.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');const MovieSchema = new mongoose.Schema({ title: { type: String, required: true }, genre: { type: String, required: true }, year: { type: Number, required: true } });module.exports = mongoose.model('Movie', MovieSchema);
